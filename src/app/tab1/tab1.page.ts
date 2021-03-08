5e69cb48db4adca1e7690dfaeefbfc8af8bff6f7
puliwang@cxist.com <puliwang@cxist.com>
2021-03-08 18:23:53 +0800

    修改初始项目

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
-    selector: 'app-tab1',
-    templateUrl: 'tab1.page.html',
-    styleUrls: ['tab1.page.scss']
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
     public constructor(
+        private navCtl: NavController,
+        private router: Router,
+        private acr: ActivatedRoute
     ) { }
+
+    public goto(name: string) {
+        if (name === 'student') {
+            // this.navCtl.navigateForward('');
+            this.router.navigate(['./student'], { relativeTo: this.acr });
+        } else {
+            this.router.navigate(['./teacher'], { relativeTo: this.acr });
+        }
+
+      
+        // this.router.navigateByUrl('/tabs/tab1/home/student');
+    }
 
 }
--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -8,13 +8,13 @@ import { Tab3Page } from './tab3.page';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }]),
-    Tab3PageRoutingModule,
-  ],
-  declarations: [Tab3Page]
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([{ path: '', component: Tab3Page }]),
+        Tab3PageRoutingModule,
+    ],
+    declarations: [Tab3Page]
 })
-export class Tab3PageModule {}
+export class Tab3PageModule { }
